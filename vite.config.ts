2993cd316ec11101ceb172f53eb6eb4d010a45b1
victordfga <victorsudden@gmail.com>
2025-10-21 19:04:44 -0500

    Update vite.config.ts
    cambiando el nombre de la base "generador-de-glosa-v2"

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/generador-glosas-siga/',
+  base: '/generador-de-glosa-v2/',
   server: {
     port: 3000,
     open: true
